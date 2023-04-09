9fec7bbdf071be3a377c843565eddd0c9c6e430a
Jacob Rutledge <jacob.rutledge0509@gmail.com>
2023-04-09 10:04:40 -0500

    Initial commit

---
+++ src/main/java/io/github/red050911/defensesystems/DefenseSystems.java
@@ -0,0 +1,12 @@
+package io.github.red050911.defensesystems;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DefenseSystems implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+}
---
+++ src/main/java/io/github/red050911/defensesystems/client/DefenseSystemsClient.java
@@ -0,0 +1,12 @@
+package io.github.red050911.defensesystems.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DefenseSystemsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
